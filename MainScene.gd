a619ec200ae713693d80071a5f7d67c11eee3ef4
EverettM12 <metivaeverett@gmail.com>
2025-04-02 21:05:59 -0400

    Update MainScene.gd

--- MainScene.gd
+++ MainScene.gd
@@ -9,16 +9,21 @@ func _process(_delta):
 	if Input.is_action_just_pressed("Pause"):
 		_pause_menu()
 
-
-
 func _pause_menu():
 	if paused:
+		# Return to captured mouse mode and hide pause menu
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		pause_menu.hide()
 		Engine.time_scale = 1
 	else:
+		# Set mouse mode to visible and show the pause menu
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		pause_menu.show()
 		Engine.time_scale = 0
 		
 	paused = !paused
+
+
+func _on_quit_pressed() -> void:
+	print("Button Pressed")
+	get_tree().quit()
